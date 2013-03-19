1fa2ff8288b30104f0d1d366149f94b3f6df0b2a
Eloy Gomez <eloy@indeos.es>
2013-03-19 17:51:21 +0100

    Send job_id param if setted

--- lib/debox/command/log.rb
+++ lib/debox/command/log.rb
@@ -3,13 +3,13 @@ require 'debox/command/base'
 class Debox::Command::Log < Debox::Command::Base
   include Debox::Utils
 
-  help :index, params: ['application'],opt_params: ['environment', 'index'],
+  help :index, params: ['application'],opt_params: ['environment', 'job_id'],
   text: "Show log. Last by default"
 
   def index
     opt = { app: args[0] }
     opt[:env] = args[1] if args.length > 1
-    opt[:index] = args[2] if args.length > 2
+    opt[:job_id] = args[2] if args.length > 2
 
     puts Debox::API.log opt
   end
